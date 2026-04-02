fcea498c48d87ec5ded3566f34910a673b6c4306
jsandoval86 <juancsandoval86@gmail.com>
2026-04-02 17:16:52 -0500

    refactor: delete requests from shared-kernel

--- packages/shared-kernel/src/requests/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./tenant.request";
-export * from "./campaign.request";
-export * from "./activity.request";
-export * from "./slide.request";
-export * from "./kiosk.request";
-export * from "./auth.request";
